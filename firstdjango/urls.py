024f360b5efb0ab6475c22cf746800eeec662786
fusionman24 <shlok18082005agrawl@gmail.com>
2025-01-23 22:28:27 +0530

    basic

--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home.views import *
+
 urlpatterns = [
+    path('',home, name='home'),
+    path('about/',about, name='about'),
     path('admin/', admin.site.urls),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    
+    for people in peoples:
+        print(people['name'])
+    
+    peoples=[{'name':'John','age':23},{'name':'Doe','age':24},{'name':'Jane','age':25},{'name':'Danisson','age':26}]
+    return render(request,'index.html',context={'peoples':peoples})
+
+    
+
+
+def about(request):
+    return HttpResponse("About Us")
